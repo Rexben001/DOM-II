9f9f9c81e3d073152bb305d051fe3ebb8cc29e3f
Rexben <rexben.rb@gmail.com>
2019-05-14 13:29:42 +0100

    use resize and scroll

--- js/index.js
+++ js/index.js
@@ -33,3 +33,34 @@ firstImage.style.display = "none";
 window.addEventListener("load", () => {
   setTimeout(() => (firstImage.style.display = "block"), 1000);
 });
+
+const contentDestination = document.querySelector('.content-destination');
+
+const navLink = document.querySelector('.nav-link:first-child');
+
+navLink.addEventListener('click', (event) => {
+    console.log(2345678)
+    event.preventDefault();
+    contentDestination.focus();
+});
+
+if (contentDestination.focus())
+{
+    contentDestination.setAttribute('style', 'color: red');
+}
+
+//resize
+window.addEventListener('resize', (event) => {
+    bodyPage.style.backgroundColor = 'rgba(0,0,0, .1)';
+});
+
+//cscroll
+window.addEventListener('scroll', () => {
+    const red = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+  
+    bodyPage.style.color = `rgb(${red},${green},${blue})`;
+
+    console.log('its working')
+});
