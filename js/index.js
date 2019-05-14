02eb532070c0fe023ecd97722c3c2e3fbccb50e8
Rexben <rexben.rb@gmail.com>
2019-05-14 12:18:19 +0100

    use mouseover and mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+const logoHeading = document.querySelector('.logo-heading');
+console.log(logoHeading);
+logoHeading.addEventListener('mouseover', () => {
+    logoHeading.setAttribute("style", "color:red; cursor:pointer; box-shadow: 0 5px 5px #000");
+});
+logoHeading.addEventListener('mouseout', () => {
+    logoHeading.setAttribute("style", "color:inherit;");
+});
