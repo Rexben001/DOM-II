3bcdf63247536c32d63677342475562a9c2a83b6
Rexben <rexben.rb@gmail.com>
2019-05-14 12:32:34 +0100

    use keydown

--- js/index.js
+++ js/index.js
@@ -1,10 +1,22 @@
 // Your code goes here
 
+// mouseover and mouseout
 const logoHeading = document.querySelector('.logo-heading');
-console.log(logoHeading);
 logoHeading.addEventListener('mouseover', () => {
     logoHeading.setAttribute("style", "color:red; cursor:pointer; box-shadow: 0 5px 5px #000");
 });
 logoHeading.addEventListener('mouseout', () => {
     logoHeading.setAttribute("style", "color:inherit;");
+});
+
+
+const bodyPage = document.querySelector('body');
+bodyPage.addEventListener('keydown', (event) => {
+    if (event.key === '1')
+    {
+        bodyPage.style.backgroundColor = 'rgba(0,0,0,.3)';
+    } else
+    {
+        bodyPage.style.backgroundColor = 'inherit';
+    }
 });
